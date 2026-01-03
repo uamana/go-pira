05ed40725a87d3834384bb1913c44dfdc32176fc
Sergey Juriev <sjuriev@gmail.com>
2026-01-03 22:13:40 +0300

    Move common code for memory and terminal interfaces into pira.go

--- pkg/pira/terminal.go
+++ pkg/pira/terminal.go
@@ -1,75 +1,12 @@
 package pira
 
 import (
-	"bufio"
 	"bytes"
 	"fmt"
 	"log/slog"
-	"time"
 
 	"go.bug.st/serial"
 )
-
-type Pira struct {
-	port     string
-	baudRate int
-	conn     serial.Port
-	reader   *bufio.Reader
-}
-
-func Dial(port string, baudRate int, timeout time.Duration) (*Pira, error) {
-	conn, err := serial.Open(port, &serial.Mode{BaudRate: baudRate})
-	if err != nil {
-		return nil, fmt.Errorf("failed to open port %s: %w", port, err)
-	}
-	err = conn.SetReadTimeout(timeout)
-	if err != nil {
-		return nil, fmt.Errorf("failed to set read timeout: %w", err)
-	}
-	return &Pira{
-		port:     port,
-		baudRate: baudRate,
-		conn:     conn,
-		reader:   bufio.NewReader(conn),
-	}, nil
-}
-
-func (p *Pira) Close() error {
-	return p.conn.Close()
-}
-
-func (p *Pira) SendCommand(command Command) (int, error) {
-	n, err := p.conn.Write([]byte(command))
-	if err != nil {
-		return 0, err
-	}
-	err = p.conn.Drain()
-	if err != nil {
-		return 0, err
-	}
-	return n, nil
-}
-
-func (p *Pira) RecvResponse() ([]byte, error) {
-	buf := make([]byte, 0, 1024)
-	wb := bytes.NewBuffer(buf)
-
-	lineCount := 0
-	for {
-		line, err := p.reader.ReadBytes('\n')
-		if err != nil {
-			return nil, err
-		}
-		_, err = wb.Write(line)
-		lineCount++
-		if err != nil {
-			return nil, err
-		}
-		if bytes.Equal(line, []byte("\r\n")) && lineCount > 2 {
-			return wb.Bytes(), nil
-		}
-	}
-}
 
 func (p *Pira) GetBasicData() (*BasicData, error) {
 	n, err := p.SendCommand(CmdGetBasicData)
